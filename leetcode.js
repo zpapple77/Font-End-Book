a40262db9f3d50b06f5c8d4776f21013b267fe12
Pinger <zpapple77@163.com>
2022-04-07 08:32:32 +0800

    定位

--- leetcode.js
+++ leetcode.js
@@ -1,25 +1,6 @@
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val
-  this.next = next === undefined ? null : next
+var test =function(){
+  console.log('js');
+
 }
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var sortList = function (head) {
-  let dummy = new ListNode(0, head)
-  let q = head.next
-  while (q !== null) {
-    let p = dummy
-    while (p !== null && p.next !== q) {
-      if (p.next.val > q.val) {
-        let tmp = q.next
-        q.next = p.next
-        p.next = q
-        q = tmp
-        break
-      }
-    }
-  }
-  return dummy.next
-}
+
+console.log('1',test());
