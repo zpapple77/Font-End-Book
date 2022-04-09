d5d3ff1f50269ccf9f17f77f6f2148ea39a7dc7a
Pinger <zpapple77@163.com>
2022-04-09 13:52:07 +0800

    基础布局完成

--- leetcode.js
+++ leetcode.js
@@ -1,6 +1,20 @@
-var test =function(){
-  console.log('js');
-
+const _quickSort = array => {
+    // 补全代码
+    if(array.length<=1){
+        return array
+    }
+    var midIndex = Math.floor(array.length/2)
+    var midVal = array.splice(midIndex,1)[0]
+    var left = []
+    var right = []
+    for(let i=0,len = array.length;i<len;i++){
+        if(array[i]<midVal){
+            left.push(array[i])
+        } else {
+            right.push(array[i])
+        }
+    }
+    return _quickSort(left).concat([midVal],_quickSort(right))
 }
 
-console.log('1',test());
+console.log(_quickSort([5,9,6,5,4,5,6,-1,-9,9]));
